c8c7170ba5ad6b9a4ae5b349f69554e34b78aafb
Chris Whitten <152309917+cgwhitten@users.noreply.github.com>
2024-01-17 08:32:19 -0600

    Helper function added
    Added helper function to getBooksPossessedByAccount

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -21,18 +21,18 @@ function getTotalNumberOfBorrows(account, books) {
 function getBooksPossessedByAccount(account, books, authors) {
   let result = []
   const idCheck = account.id
+  function findAuthor(book, authors) {
+    let authorName = authors.find((author) => author.id === book.authorId)
+    return authorName
+    } 
   for (let i = 0; i < books.length; i++) {
     let borrowCheck = books[i].borrows;
     const borrowByBook = borrowCheck.filter((borrower) => borrower.id === idCheck);
     let stillCheckedOut = borrowByBook.filter((book) => book.returned === false);
     if (stillCheckedOut.length > 0) {
+      books[i].author = findAuthor(books[i], authors);
       result.push(books[i])
     }
-  }
-  for (let j = 0; j < result.length; j ++) {
-    let authorCheck = result[j].authorId
-    let authorToAdd = authors.find((author) => author.id === authorCheck)
-    result[j].author = authorToAdd
   }
   return result
 }
